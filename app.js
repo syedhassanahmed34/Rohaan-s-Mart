593117e901d27dd2190b570daa0b01e5308dda9c
Syedhassanahmed534 <shassanahmed47@gmail.com>
2024-05-03 19:52:37 +0500

    JAVASCRIPT

---
+++ app.js
@@ -0,0 +1,4 @@
+function showmessage(){
+    alert(' Sorry for disavailiability i am currently working on this\n I make sure that this will be working in next few months!!\n Regards ---> Hassan ')
+
+}
